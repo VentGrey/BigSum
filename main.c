efca25f90bb200eddc6fa97336c22fc811f2a241
VentGrey <omarpurataf@gmail.com>
2018-10-31 18:22:09 -0600

    intial C func

---
+++ main.c
@@ -0,0 +1,26 @@
+/* Instrucciones:
+ * Calcule e imprima la suma de los elementos dentro de un arreglo, siempre
+ * teniendo en cuenta que algunos de los enteros ingresados pueden ser
+ * enormes.
+ *
+ * Parámetos de la función:
+ * ar: Un arreglo de enteros
+ *
+ * Formato de entrada:
+ * La primera línea de entrada consiste en un entero n.
+ * La segunda línea contiene n enteros separados por un espacio dentro del
+ * arreglo.
+ *
+ * Formato de salida:
+ * La suma de los enteros del arreglo :v
+ *
+ * Condiciones
+ * 1 <= n <= 10
+ * 10 <= ar[i] <= 10^10
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+}
